f53cc55402a4d99e4bf514e195e28d2325227e2e
ysr <141933879+jysr0@users.noreply.github.com>
2025-08-01 18:55:53 +0100

    Add files via upload

--- blocks.h
+++ blocks.h
@@ -3,12 +3,20 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval in s*/	/*Update Signal*/
 	{"┃ ram: ",	"free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	30,		0}, /* default after print: $3\"/\"$2 */ 
 	{"cpu: ",	"sensors | tr -d '+' | awk 'NR == 3 { print $4 }'",	30,	0},	
+
+	{"💽 ",	"df -h | awk 'NR==5 { print $5 }'",	3600,	0},    
+	{"💾 ",	"df -h | awk 'NR==11 { print $5 }'",	3600,	0},    
+
       /*{"⌨ ",	"a.out | awk 'NR == 2 { print $3 }'",	1,	0},*/	
-	{"🔋",    "acpi -b | awk --field-separator=, '{ print $2 }'",	120,	0},	
+      
+      /*{"🔋",    "acpi -b | awk --field-separator=, '{ print $2 }'",	120,	0},*/		
+	{"🔋 ",    "battery.block",	120,	0},	
       /*{"🔆 ",	"brightnessctl | tr -d '()' | awk 'NR == 2 { print $4 }'",	0,	2},*/		
-        {"🔊 ",	"pactl get-sink-volume @DEFAULT_SINK@ | awk '{ print $5 }'",	0,	5},				
+        {"🔊 ",	"volume.block",	0,	5},				
+	
 	{"🛜 ",	"nmcli g | awk 'NR == 2 {print $1}'",	5,	0},		
       /*{"up: ",	"uptime | awk '{print $3}' | tr -d ,",	60,	0},*/	
+	
 	{"📆 ", "date '+%m.%d'",					86400,		0},
 	{"⏰ ", "date '+%R'",					60,		0},
 };
