c968a74e77651c46ddf7af636b6a0aa1e6951926
ysr <141933879+jysr0@users.noreply.github.com>
2026-01-03 11:54:13 +0100

    Add files via upload

--- blocks.def.copy.h
+++ blocks.def.copy.h
@@ -1,23 +1,23 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/	/*Command*/		/*Update Interval in s*/	/*Update Signal*/
-    //{"ROOT: ",	"df -h | awk 'NR==5 { print $5 }'",	3600,	0},
-    //{"HOME: ",	"df -h | awk 'NR==11 { print $5 }'",	3600,	0},
-    //{"IP: ",	"ip -f inet address | awk 'NR==6 { print $2 }'",	10,	0},
-    //{"KeyB: ",	"a.out | awk 'NR==2 { print $3 }'",	1,	0},
+    /*Icon*/	/*Command*/		/*Update Interval in s*/	/*Update Signal (1-31)*/
+    //{"ROOT: ",	"df -h | awk 'NR==5 { print $5 }'",				3600,	0},
+    //{"HOME: ",	"df -h | awk 'NR==11 { print $5 }'",			3600,	0},
+    //{"IP: ",	"ip -f inet address | awk 'NR==6 { print $2 }'",	10,		0},
+    //{"KeyB: ",	"a.out | awk 'NR==2 { print $3 }'",				1,		0},
 
-    //{" ",	"dwmb_ram.sh",	120,		1}, 
-    {" ",	"dwmb_cpu.sh",	120,	3},	
-    {"",	"dwmb_network2.sh",	10,	4},		
-    //{"",	"dwmb_rxtx.sh",	10,	0},
-    {"",    "dwmb_battery1.sh",	3,	6},	
-    {"",	"dwmb_brightness.sh",	0,	2},
-    {"",	"dwmb_volume.sh",	0,	5},				
-    {"",	"dwmb_kb_layout.sh",	0,	9},				
-    {"", 	"dwmb_clock.sh",	60,	7},
-    //{"", 	"dwmb_kernel.sh",	3600,	0},
-    //{"",	"dwmb_uptime.sh",	600,	8},
-    //{"",	"dwmb_playing.sh",	3,	0},
+    //{" ",	"dwmb_ram.sh",								120,		3}, 
+    {" ",	"dwmb_cpu.sh",								120,		1},	
+    {"",	"dwmb_network2.sh",							10,			4},		
+    //{"",	"dwmb_rxtx.sh",								10,			0},
+    {"",    "dwmb_battery1.sh",							3,			8},	
+    {"",	"dwmb_brightness.sh",						0,			2},
+    {"",	"dwmb_volume.sh",							0,			5},				
+    {"",	"dwmb_kb_layout.sh",						0,			9},				
+    {"", 	"dwmb_clock.sh",							60,			7},
+    //{"", 	"dwmb_kernel.sh",							3600,		0},
+    //{"",	"dwmb_uptime.sh",							600,		0},
+    //{"",	"dwmb_playing.sh",							3,			0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
